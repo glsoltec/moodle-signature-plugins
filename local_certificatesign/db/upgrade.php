002df869122e70ff55782049eefa18f0ccf2cc81
root <root@vmi3018606.contaboserver.net>
2026-07-29 01:10:20 -0300

    fix: upgrade resiliente - cria tabela log se nao existir
    upgrade.php 2026073000 agora verifica table_exists() antes de tentar
    adicionar campos. Se a tabela nao foi criada pela migracao 2026071800
    (instalacao corrompida, upgrade falhou, etc), cria a tabela completa
    com todos os 7 campos (originais + email_*).
    
    Evita erro 'ddltablenotexist' em ambientes onde a tabela foi perdida.

--- local_certificatesign/db/upgrade.php
+++ local_certificatesign/db/upgrade.php
@@ -57,15 +57,28 @@ function xmldb_local_certificatesign_upgrade(int $oldversion): bool {
 
     if ($oldversion < 2026073000) {
         $table = new \xmldb_table('local_certificatesign_log');
-        $fields = [
-            new \xmldb_field('email_sent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'timecreated'),
-            new \xmldb_field('email_time', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'email_sent'),
-            new \xmldb_field('email_attempts', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, '0', 'email_time'),
-            new \xmldb_field('email_last_error', XMLDB_TYPE_CHAR, '255', null, null, null, null, 'email_attempts'),
-        ];
-        foreach ($fields as $field) {
-            if (!$dbman->field_exists($table, $field)) {
-                $dbman->add_field($table, $field);
+        if (!$dbman->table_exists($table)) {
+            $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE);
+            $table->add_field('issueid', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL);
+            $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, '0');
+            $table->add_field('email_sent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, '0');
+            $table->add_field('email_time', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, '0');
+            $table->add_field('email_attempts', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, '0');
+            $table->add_field('email_last_error', XMLDB_TYPE_CHAR, '255', null, null, null, '');
+            $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
+            $table->add_key('issueid', XMLDB_KEY_UNIQUE, ['issueid']);
+            $dbman->create_table($table);
+        } else {
+            $fields = [
+                new \xmldb_field('email_sent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'timecreated'),
+                new \xmldb_field('email_time', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, '0', 'email_sent'),
+                new \xmldb_field('email_attempts', XMLDB_TYPE_INTEGER, '4', null, XMLDB_NOTNULL, null, '0', 'email_time'),
+                new \xmldb_field('email_last_error', XMLDB_TYPE_CHAR, '255', null, null, null, null, 'email_attempts'),
+            ];
+            foreach ($fields as $field) {
+                if (!$dbman->field_exists($table, $field)) {
+                    $dbman->add_field($table, $field);
+                }
             }
         }
         upgrade_plugin_savepoint(true, 2026073000, 'local', 'certificatesign');
